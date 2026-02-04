ae55bdad107f251a9824ed5e26e41b4d5cc2a508
Jabbath <antonafana@yahoo.ca>
2026-02-04 21:59:52 +1300

    Fixed directories and allowed parallelization.

--- paper/generate_transitions.sh
+++ paper/generate_transitions.sh
@@ -3,23 +3,32 @@ export PYTHONUNBUFFERED=1
 export NUMBA_CUDA_USE_NVIDIA_BINDING=0
 
 i=0
-cell_budget=20000
+MAX_JOBS=7
+
+mkdir -p data_dir/transitions/
 
 for num_gen in {1..25}; do
+  # Block until fewer than MAX_JOBS are running
+  while [ "$(jobs -rp | wc -l)" -ge "$MAX_JOBS" ]; do
+    wait -n
+  done
+
   python simulate_fly_breeding.py \
-    --transition_save_path "datasets/transitions/transitions_{$num_gen}_gen.pickle" \
-    --save_dir "sweeps/gen/" \
+    --transition_save_path "data_dir/transitions/transitions_{$num_gen}_gen.pickle" \
+    --save_dir "sweeps/transitions/" \
     --run_num "$i" \
     --num_repeats 30 \
     --num_generations $num_gen \
     --avg_UMI 100 \
     --num_cells_per_org 1 \
     --offspring_per_generation 1000 \
-    --num_threads 80 \
+    --num_threads 22 \
     --no_demuxHMM \
     --no_souporcell \
     --no_vireo \
-    --no_scsplit
+    --no_scsplit &
 
   ((i++))
 done
+
+wait
